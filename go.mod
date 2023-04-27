b5f6fc299cbf95b08e3b579add7dac4204e607fa
Roy Ramírez <roydramirezv@hotmail.com>
2023-04-26 20:20:42 -0600

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rdramirezv/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/rdramirezv/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
